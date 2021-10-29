23a2bc8182d90be82842429d241cef3f8fe47ad4
Maksim <max1983@inbox.lv>
2021-10-29 21:20:00 +0300

    card2 added

---
+++ src/components/Cards/Card2.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './cardOne.module.css';
+
+const Card2 = () => (
+  <div className={styles.card}>
+    <h2>Моя первая карточка</h2>
+    <div>
+      Имя: Максим
+    </div>
+    <div>
+      Фронтенд-разработчик
+    </div>
+  </div>
+);
+
+export default Card2;
--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardOne from '../Cards/CardOne';
+import Card2 from '../Cards/Card2';
 
 const Examples = () => (
   <>
     <div>
       <CardOne />
+      <Card2 />
     </div>
   </>
 );
