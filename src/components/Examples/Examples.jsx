cbc4327ca56cf8a50e2435682d7e20003e5c5ec7
a-naraikin <anna.naraikin@yandex.ru>
2021-10-31 23:45:24 +0300

    add CardThree

--- src/components/Examples/Examples.jsx
+++ src/components/Examples/Examples.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CardOne from '../Cards/CardOne';
+import CardThree from '../Cards/CardThree';
 
 const Examples = () => (
   <>
     <div>
       <CardOne />
+      <CardThree />
     </div>
   </>
 );
