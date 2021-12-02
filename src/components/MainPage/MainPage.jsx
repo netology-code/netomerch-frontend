aa7fcab868a9af117661b85b9535fb7e568bfc95
Olga Runova <ockaaa@gmail.com>
2021-12-02 20:00:11 +0300

    added answers

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Answers from '../Answers/Answers';
 import styles from './mainPage.module.css';
 
 const MainPage = () => (
-  <div className={styles.mainPage}>
-    Тут ваш любимый сайт на ближайшие пару недель =)
-  </div>
+  <>
+    <div className={styles.mainPage}>
+      Тут ваш любимый сайт на ближайшие пару недель =)
+    </div>
+    <Answers />
+  </>
 );
 
 export default MainPage;
