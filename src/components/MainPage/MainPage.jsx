0d5c64a1a724b57694df2a7cd1aed7052effa198
Maksim <max1983@inbox.lv>
2021-12-02 14:43:45 +0200

    reviews

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainTop from './MainTop';
 import styles from './mainPage.module.css';
+import Reviews from './Reviews';
 
 const MainPage = () => (
   <div className={styles.mainPage}>
     <MainTop />
+    <Reviews />
   </div>
 );
 
---
+++ src/components/MainPage/Reviews/Reviews.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable max-len */
+import React from 'react';
+import './reviews.css';
+
+export default function Reviews() {
+  return (
+    <div className="reviews">
+      <div className="container">
+        <div className="title">Блок с отзывами на товары</div>
+        <div className="reviews__text">
+          Часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной рыбой для текстов на латинице с начала XVI века. В то время некий безымянный для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum.
+        </div>
+        <div className="reviews__slider">
+          Slider
+        </div>
+      </div>
+    </div>
+  );
+}
