b1f95627375243753937195775e615082f8a3637
a-naraikin <anna.naraikin@yandex.ru>
2021-12-03 23:12:21 +0300

    временно изменила img

--- src/components/MainPage/MainTop/MainTop.jsx
+++ src/components/MainPage/MainTop/MainTop.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './main-top.css';
 import '../Slider/slider.css';
 
+import img from './img/content_merch_1.png';
+
 export default function MainTop() {
   return (
     <div className="main-top">
       <div className="container">
         <div className="slider slider-main">
           <div className="slider-main__img ibg">
-            <img src="https://placeimg.com/1000/480/tech" alt="1" />
+            {/* <img src="https://placeimg.com/1000/480/tech" alt="1" /> */}
+            <img src={img} alt="1" />
           </div>
           <div className="slider__control slider-main__control">
             <button className="slider__arrow" type="button">
