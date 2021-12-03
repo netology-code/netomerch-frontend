73ea9a681e811463a1668f9a172b9a0edb2078df
a-naraikin <anna.naraikin@yandex.ru>
2021-12-03 14:56:47 +0300

    add верстка блока AboutMerch

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,11 +3,13 @@ import MainTop from './MainTop';
 import Reviews from './Reviews';
 import Answers from '../Answers/Answers';
 import MadeBy from './MadeBy/MadeBy';
+import AboutMerch from './AboutMerch/AboutMerch';
 import styles from './mainPage.module.css';
 
 const MainPage = () => (
   <div className={styles.mainPage}>
     <MainTop />
+    <AboutMerch />
     <Reviews />
     <MadeBy />
     <Answers />
