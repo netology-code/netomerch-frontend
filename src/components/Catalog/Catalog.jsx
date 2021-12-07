d2578f5b77d8313ea4431b17b90b351d37d30ca6
a-naraikin <anna.naraikin@yandex.ru>
2021-12-07 18:02:11 +0300

    add верстка CatalogItems

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import CatalogBanner from './CatalogBanner/CatalogBanner';
+import CatalogItems from './CatalogItems/CatalogItems';
 import styles from './catalog.module.css';
 
 const Catalog = () => (
   <div className={styles.catalog}>
     <CatalogBanner />
-    {
-    // <CatalogFilter />
-    // <CatalogItems />
-    }
+    {/* <CatalogFilter /> */}
+    <CatalogItems />
   </div>
 );
 
---
+++ src/components/Catalog/CatalogItems/CatalogItems.jsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import styles from './catalogItems.module.css';
+import img from '../../../assets/img/catalog_unit_content.png';
+
+export default function CatalogItems() {
+  return (
+    <section className={styles.catalogItems_content}>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой груди и руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+      <div className={styles.catalogItems_item}>
+        <div>
+          <div className={styles.catalogItems_image}>
+            <img src={img} alt="толстовка" />
+          </div>
+
+          <a className={styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
+        </div>
+
+        <div className={styles.catalogItems_block_price}>
+          <p className={styles.catalogItems_price}>2500 p.</p>
+          <span className={styles.catalogItems_cart} />
+        </div>
+      </div>
+    </section>
+  );
+}
