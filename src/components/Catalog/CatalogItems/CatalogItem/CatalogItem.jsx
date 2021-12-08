07b9057d7c52a17ef2145e46974ef9e309a0fe26
a-naraikin <anna.naraikin@yandex.ru>
2021-12-08 23:32:06 +0300

    fix: исправила верстку

--- src/components/Catalog/CatalogItems/CatalogItem/CatalogItem.jsx
+++ src/components/Catalog/CatalogItems/CatalogItem/CatalogItem.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-indent */
 /* eslint-disable no-undef */
 import React from 'react';
 import styles from './catalogItem.module.css';
@@ -5,17 +6,16 @@ import img from '../../../../assets/img/catalog_unit_content.png';
 
 const CatalogItem = () => (
   <div className={styles.catalogItem_item}>
-    <div>
-      <div className={`${styles.catalogItem_image} ${styles.ibg}`}>
-        <img src={img} alt="1" />
-      </div>
-
-      <a className={styles.catalogItem_link} href="#0">Худи с принтом на правой руке</a>
+    <div className={`${styles.catalogItem_image} ibg`}>
+      <img src={img} alt="1" />
     </div>
 
-    <div className={styles.catalogItem_block_price}>
-      <p className={styles.catalogItem_price}>2500 p.</p>
-      <span className={styles.catalogItem_cart} />
+    <div className={styles.catalogItem_footer}>
+      <a className={styles.catalogItem_link} href="#0">Худи с принтом на правой руке</a>
+      <div className={styles.catalogItem_block_price}>
+        <p className={styles.catalogItem_price}>2500 p.</p>
+        <span className={styles.catalogItem_cart} />
+      </div>
     </div>
   </div>
 );
