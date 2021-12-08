50892b480db227a46fc2f0228fe22f2c59fcae90
a-naraikin <anna.naraikin@yandex.ru>
2021-12-08 12:28:04 +0300

    разделила создание item и CatalogItems

---
+++ src/components/Catalog/CatalogItems/CatalogItem/CatalogItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './catalogItem.module.css';
+import img from '../../../../assets/img/catalog_unit_content.png';
+
+const CatalogItem = () => (
+  <div className={styles.catalogItem_item}>
+    <div>
+      <div className={styles.catalogItem_image}>
+        <img src={img} alt="1" />
+      </div>
+
+      <a className={styles.catalogItem_link} href="#0">Худи с принтом на правой руке</a>
+    </div>
+
+    <div className={styles.catalogItem_block_price}>
+      <p className={styles.catalogItem_price}>2500 p.</p>
+      <span className={styles.catalogItem_cart} />
+    </div>
+  </div>
+);
+
+export default CatalogItem;
--- src/components/Catalog/CatalogItems/CatalogItems.jsx
+++ src/components/Catalog/CatalogItems/CatalogItems.jsx
@@ -1,46 +1,21 @@
-/* eslint-disable no-const-assign */
-/* eslint-disable no-plusplus */
 import React from 'react';
 import styles from './catalogItems.module.css';
-import img from '../../../assets/img/catalog_unit_content.png';
+import CatalogItem from './CatalogItem/CatalogItem';
 
-function CreateItems() {
-  const classNameContent = `.${styles.catalogItems_content}`;
-  const content = document.querySelector(classNameContent);
-
-  const classNameItem = `.${styles.catalogItems_item}`;
-  const items = content.querySelectorAll(classNameItem);
-
-  for (let i = items.length; i < 10; i++) {
-    content.innerHTML += `
-      <div class=${styles.catalogItems_item}>
-        <div>
-          <div class=${styles.catalogItems_image}>
-            <img src=${img} alt="толстовка" />
-          </div>
-
-          <a class=${styles.catalogItems_link} href="#0">Худи с принтом на правой руке</a>
-        </div>
-
-        <div class=${styles.catalogItems_block_price}>
-          <p class=${styles.catalogItems_price}>2500 p.</p>
-          <span class=${styles.catalogItems_cart} />
-        </div>
-      </div>`;
-  }
-}
-
-setTimeout(CreateItems, 1000);
+const mockData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 export default function CatalogItems() {
   return (
-    <section className={styles.catalogItems_wrapper}>
+    <div className={styles.catalogItems_wrapper}>
       <div className={`${styles.catalogItems_star} ${styles.catalogItems_star__1}`} />
       <div className={`${styles.catalogItems_star} ${styles.catalogItems_star__2}`} />
       <div className={`${styles.catalogItems_star} ${styles.catalogItems_star__3}`} />
       <div className={`${styles.catalogItems_star} ${styles.catalogItems_star__4}`} />
+      <div className={`${styles.catalogItems_star} ${styles.catalogItems_star__5}`} />
 
-      <div className={styles.catalogItems_content} />
-    </section>
+      <div className={styles.catalogItems_content}>
+        { mockData.map(() => <CatalogItem />) }
+      </div>
+    </div>
   );
 }
