613d6002d3d5314c9a8f76eaa5497f1b0dd6b3b8
a-naraikin <anna.naraikin@yandex.ru>
2021-12-09 23:16:48 +0300

    add ссылку к картинке и корзине в item

--- src/components/Catalog/CatalogItems/CatalogItem/CatalogItem.jsx
+++ src/components/Catalog/CatalogItems/CatalogItem/CatalogItem.jsx
@@ -1,20 +1,20 @@
-/* eslint-disable react/jsx-indent */
-/* eslint-disable no-undef */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from 'react';
 import styles from './catalogItem.module.css';
 import img from '../../../../assets/img/catalog_unit_content.png';
 
 const CatalogItem = () => (
   <div className={styles.catalogItem_item}>
-    <div className={`${styles.catalogItem_image} ibg`}>
+    <a className={`${styles.catalogItem_image} ibg`} href="#0">
       <img src={img} alt="1" />
-    </div>
+    </a>
 
     <div className={styles.catalogItem_footer}>
       <a className={styles.catalogItem_link} href="#0">Худи с принтом на правой руке</a>
       <div className={styles.catalogItem_block_price}>
         <p className={styles.catalogItem_price}>2500 p.</p>
-        <span className={styles.catalogItem_cart} />
+        <a className={styles.catalogItem_cart} href="#0" />
       </div>
     </div>
   </div>
