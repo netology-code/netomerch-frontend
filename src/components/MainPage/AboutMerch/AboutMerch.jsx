fe6c20c7634262a23c8be438c1a838dd27780054
kxrxll <kovalyovkirill@gmail.com>
2021-12-15 01:03:52 +0300

    renderdata

--- src/components/MainPage/AboutMerch/AboutMerch.jsx
+++ src/components/MainPage/AboutMerch/AboutMerch.jsx
@@ -5,40 +5,23 @@
 import React from 'react';
 import styles from './aboutMerch.module.css';
 import Title from '../../ui/Title';
-import img2 from './img/content_merch_2.jpg';
-import img3 from './img/content_merch_3.jpg';
-import img4 from './img/content_merch_4.jpg';
 
 export default function AboutMerch({ popular }) {
-  console.log(popular);
   return (
     <section className={styles.aboutMerch_section}>
       <div className="container">
         <div className={styles.aboutMerch_body}>
           <Title cn={styles.aboutMerch_header} text="информационный блок о мерче" sqColor="pink" />
           <p className={styles.aboutMerch_text}>Часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum,</p>
-
           <div className={styles.aboutMerch_items}>
-            <div className={styles.aboutMerch_item}>
-              <div className={styles.aboutMerch_image}>
-                <img src={img2} alt="толстовка" />
+            {popular.map((item) => (
+              <div className={styles.aboutMerch_item} key={item.id}>
+                <div className={styles.aboutMerch_image}>
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <a className={styles.aboutMerch_link} href="#0">{item.name}</a>
               </div>
-              <a className={styles.aboutMerch_link} href="#0">наименование позиции 1</a>
-            </div>
-
-            <div className={styles.aboutMerch_item}>
-              <div className={styles.aboutMerch_image}>
-                <img src={img3} alt="футболка" />
-              </div>
-              <a className={styles.aboutMerch_link} href="#0">наименование позиции 1</a>
-            </div>
-
-            <div className={styles.aboutMerch_item}>
-              <div className={styles.aboutMerch_image}>
-                <img src={img4} alt="блокноты" />
-              </div>
-              <a className={styles.aboutMerch_link} href="#0">наименование позиции 1</a>
-            </div>
+            ))}
           </div>
 
           <div className={styles.aboutMerch_star} />
