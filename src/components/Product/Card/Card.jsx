a7a8c6ab6865a2c4cce9e8fca2d45febe4718e03
Maksim <max1983@inbox.lv>
2021-12-18 14:03:26 +0200

    feat: card logic added

--- src/components/Product/Card/Card.jsx
+++ src/components/Product/Card/Card.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 /* eslint-disable no-shadow */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
@@ -6,64 +7,126 @@ import React, { useState, useEffect } from 'react';
 import Title from '../../ui/Title';
 import './card.css';
 
+const regCount = /^\d{0,3}$/;
+
 export default function Card({ mockData }) {
-  // fetch('https://dev.netomerch.tk/api/v1/card/2')
+  // fetch('https://dev.netomerch.tk/api/v1/card/1')
   //   .then((response) => response.json())
   //   .then((data) => console.log('data', data));
 
-  const { colors } = mockData;
-  const [currColor, setCurrColor] = useState({});
-  // const colorMain =
-  // const imgMain =
+  const { name, description, price, colors, sizes } = mockData;
+  const [currColor, setCurrColor] = useState(colors.find((item) => item.is_main));
+  const [currImg, setCurrImg] = useState('');
+  const [currSize, setCurrSize] = useState(null);
+  const [currCount, setCurrCount] = useState(0);
+  const [isMsgAdd, setIsMsgAdd] = useState(false);
 
   useEffect(() => {
-    const colorMain = colors.find((item) => item.is_main);
-    setCurrColor(colorMain);
-    console.log('colorMain', colorMain);
-  }, []);
+    setCurrImg(currColor.images.find((image) => image.is_main).image);
+  }, [currColor]);
 
   const handleOnColorClick = (color) => {
     if (color.color_code === currColor.color_code) return;
-    console.log('handleOnColorClick', color.name);
     setCurrColor(color);
+  };
+
+  const handleOnImgClick = (image) => {
+    if (image === currImg) return;
+    setCurrImg(image);
+  };
+
+  const handleOnSizeClick = (size) => {
+    if (size === currSize) setCurrSize(null);
+    else setCurrSize(size);
+  };
+
+  const handleOnCountIncClick = () => {
+    if (currCount > 998) return;
+    setCurrCount(parseInt(currCount, 10) + 1);
+  };
+
+  const handleOnCountDecClick = () => {
+    if (currCount < 1) return;
+    setCurrCount(parseInt(currCount, 10) - 1);
+  };
+
+  const handleOnCountChange = (event) => {
+    const { value } = event.target;
+
+    if (!regCount.test(value)) return;
+    if (value === '') setCurrCount('');
+    else setCurrCount(parseInt(value, 10));
+  };
+
+  const handleOnCountBlur = (event) => {
+    if (event.target.value === '') setCurrCount('0');
+  };
+
+  const handleOnAddToCartClick = (event) => {
+    event.preventDefault();
+    if (isMsgAdd) return;
+    if (!currSize) {
+      console.log('Размер не выбран!');
+      return;
+    }
+
+    if (currCount === 0) {
+      console.log('Количество - 0!');
+      return;
+    }
+
+    console.log('Add to cart');
+    setCurrCount(0);
+    setCurrSize(null);
+    setCurrColor(colors.find((item) => item.is_main));
+    setIsMsgAdd(true);
+    setTimeout(() => setIsMsgAdd(false), 3000);
   };
 
   return (
     <div className="card">
       <div className="container">
-        <Title cn="card__title" text="Наименование товара" sqColor="purple" />
+        <Title cn="card__title" text={name} sqColor="purple" />
         <div className="card__content">
           <div className="card__column-1">
             <div className="card__img-main ibg">
-              <img src="https://placeimg.com/360/500/tech" alt="" />
+              <img src={currImg} alt={`Главная картинка. ${name}`} />
             </div>
 
             <div className="card__imgs">
-              <div className="card__img ibg">
-                <img src="https://placeimg.com/260/200/tech" alt="" />
-              </div>
-              <div className="card__img ibg">
-                <img src="https://placeimg.com/265/200/tech" alt="" />
-              </div>
-              <div className="card__img ibg">
-                <img src="https://placeimg.com/270/200/tech" alt="" />
-              </div>
-              <div className="card__img ibg">
-                <img src="https://placeimg.com/275/200/tech" alt="" />
-              </div>
+              {currColor.images.map((image) => (
+                <div className="card__img-wrapp img-wrapp-card">
+                  <button
+                    className={`img-wrapp-card__btn${image.image === currImg ? ' img-wrapp-card__btn_selected' : ''}`}
+                    onClick={() => handleOnImgClick(image.image)}
+                    type="button"
+                  >
+                    <div className="card__img card__img_selected ibg">
+                      <img src={image.image} alt={name} />
+                    </div>
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
 
           <div className="card__column-2">
-            <div className="card__description">{mockData.description}</div>
+            <div className="card__description">{description}</div>
 
             <div className="card__actions actions-card">
               <div className="actions-card__row-1">
                 <div className="actions-card__item card-sizes">
-                  <button className="actions-card__btn card-sizes__btn card-sizes__btn_selected" type="button">s</button>
-                  <button className="actions-card__btn card-sizes__btn" type="button">m</button>
-                  <button className="actions-card__btn card-sizes__btn" type="button">l</button>
+                  {sizes.map((size) => (
+                    <button
+                      className={`actions-card__btn card-sizes__btn${size === currSize ? ' card-sizes__btn_selected' : ''}`}
+                      type="button"
+                      onClick={() => handleOnSizeClick(size)}
+                    >
+                      {size}
+                    </button>
+                  ))}
                 </div>
+
                 <div className="actions-card__item card-colors">
                   {colors.map((color) => (
                     <button
@@ -75,25 +138,23 @@ export default function Card({ mockData }) {
                       <span className="visually-hidden">{color.name}</span>
                     </button>
                   ))}
-                  {/* <button className="actions-card__btn card-colors__btn card-colors__btn_selected" type="button"><span className="visually-hidden">{currColor.name}</span></button>
-                  <button className="actions-card__btn card-colors__btn" type="button"><span className="visually-hidden">{currColor.name}</span></button>
-                  <button className="actions-card__btn card-colors__btn" type="button"><span className="visually-hidden">{currColor.name}</span></button> */}
                 </div>
               </div>
 
               <div className="actions-card__row-2">
-                <a className="card__btn btn" href="/#">Добавить в корзину</a>
+                <a className="card__btn-add btn" href="/#" onClick={handleOnAddToCartClick}>Добавить в корзину</a>
+                <div className={`card__message${isMsgAdd ? ' card__message_active' : ''}`}>Товар добавлен в корзину</div>
                 <div className="card__count count-card">
-                  <button className="count-card__item count-card__btn" type="button">-</button>
-                  <input className="count-card__item count-card__input" value="1" />
-                  <button className="count-card__item count-card__btn" type="button">+</button>
+                  <button className="count-card__item count-card__btn" type="button" onClick={handleOnCountDecClick}>-</button>
+                  <input className="count-card__item count-card__input" value={currCount} onChange={handleOnCountChange} onBlur={handleOnCountBlur} />
+                  <button className="count-card__item count-card__btn" type="button" onClick={handleOnCountIncClick}>+</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <footer className="card__footer">
-          <div className="card__price">2500 ₽</div>
+          <div className="card__price">{`${price} ₽`}</div>
         </footer>
       </div>
     </div>
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,15 +3,33 @@ import styles from './product.module.css';
 import ProductReviews from './ProductReviews/ProductReviews';
 
 import mockData from './mockDataProductReviews.json';
-import ProductTop from './ProductTop/ProductTop';
+// import ProductTop from './ProductTop/ProductTop';
 import Card from './Card';
+
+/* Заглушка для фото ---------------------------------------------------------- */
+import imgs from './Card/TempPhoto/imgs';
+
+mockData.colors[0].images[0].image = imgs.white1;
+mockData.colors[0].images[1].image = imgs.white2;
+mockData.colors[0].images[2].image = imgs.white3;
+mockData.colors[0].images[3].image = imgs.white4;
+
+mockData.colors[1].images[0].image = imgs.black1;
+mockData.colors[1].images[1].image = imgs.black2;
+mockData.colors[1].images[2].image = imgs.black3;
+mockData.colors[1].images[3].image = imgs.black4;
+
+mockData.colors[2].images[0].image = imgs.peach1;
+mockData.colors[2].images[1].image = imgs.peach2;
+mockData.colors[2].images[2].image = imgs.peach3;
+/* ---------------------------------------------------------------------------- */
 
 const Product = () => {
   const { reviews } = mockData;
 
   return (
     <div className={styles.product}>
-      <ProductTop />
+      {/* <ProductTop /> */}
       <Card mockData={mockData} />
       <ProductReviews reviews={reviews} />
     </div>
