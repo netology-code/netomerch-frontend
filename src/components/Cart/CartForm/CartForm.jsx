9c8a26c6dba740cd7fde82126d995b7d607602c8
kxrxll <kovalyovkirill@gmail.com>
2021-12-23 22:58:40 +0300

    draft_cart

---
+++ src/components/Cart/CartForm/CartForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './cartForm.module.css';
+
+const CartForm = () => (
+  <div className={styles.form}>
+    <p>ТУТ ФОРМА</p>
+  </div>
+);
+
+export default CartForm;
