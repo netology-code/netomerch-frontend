1c883ac9f3b5d1ecc43e27546e8b6947b3c1ad6b
kxrxll <kovalyovkirill@gmail.com>
2021-12-26 21:06:33 +0300

    handling_empty_cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Title from '../ui/Title';
 import CartItem from './CartItem/CartItem';
@@ -9,11 +10,21 @@ import '../Product/Card/card.css';
 const Cart = () => {
   const { products } = useSelector((state) => state.productInCart);
   return (
-    <div className={styles.cart}>
-      <Title cn={styles.cart_header} text="ваша корзина" sqColor="pink" />
-      {products.map((item) => <CartItem key={item.id} cartItem={item} />)}
-      <CartForm />
-    </div>
+    <>
+      { products[0] !== undefined ? (
+        <div className={styles.cart}>
+          <Title cn={styles.cart_header} text="ваша корзина" sqColor="pink" />
+          {products.map((item) => <CartItem key={item.id} cartItem={item} />)}
+          <CartForm />
+        </div>
+      )
+        : (
+          <div className={styles.cart}>
+            <Title cn={styles.cart_header} text="ваша корзина пуста" sqColor="pink" />
+            <Link className={styles.btn} to="/catalog">В каталог</Link>
+          </div>
+        )}
+    </>
   );
 };
 
