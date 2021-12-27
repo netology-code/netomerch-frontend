cd228d0a8b398bf6130ab939e71475e7b3b4b58b
a-naraikin <anna.naraikin@yandex.ru>
2021-12-27 06:11:09 +0300

    add form validation

--- src/components/Cart/CartForm/CartForm.jsx
+++ src/components/Cart/CartForm/CartForm.jsx
@@ -1,60 +1,301 @@
+/* eslint-disable no-debugger */
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
+/* eslint-disable consistent-return */
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-case-declarations */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+/* eslint-disable no-unused-expressions */
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable max-len */
+/* eslint-disable default-case */
+/* eslint-disable indent */
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-lonely-if */
+/* eslint-disable no-undef */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-shadow */
+import React, { useState, useEffect } from 'react';
 import styles from './cartForm.module.css';
 import Title from '../../ui/Title';
 
-const CartForm = () => (
-  <div className={styles.form}>
-    <form action="#" id="form" className={styles.form_body}>
-      <Title cn={styles.form__title} text="ещё совсем чуть-чуть и мерч твой" sqColor="green" />
-      <div className={styles.form_wrapper}>
-        <div className={styles.form_wrapper__col}>
-          <div className={styles.form__item}>
-            <label htmlFor="forName" className={styles.form__label}>Имя и фамилия *</label>
-            <input id="forName" type="name" name="name" className={styles.form__input} />
+const useValidation = (value, validations) => {
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [minLengthError, setMinLengthError] = useState(false);
+  const [maxLengthError, setMaxLengthError] = useState(false);
+  const [firstOrLastNameError, setFirstOrLastNameError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const [inputValid, setInputValid] = useState(false);
+
+  useEffect(() => {
+    for (const validation in validations) {
+      switch (validation) {
+        case 'isEmpty':
+          value ? setIsEmpty(false) : setIsEmpty(true);
+          break;
+        case 'minLength':
+          value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false);
+          break;
+        case 'maxLength':
+          value.length > validations[validation] ? setMaxLengthError(true) : setMaxLengthError(false);
+          break;
+        case 'isfirstOrLastName':
+          const re = /^([А-ЯA-Z]|[А-ЯA-Z][\x27а-яa-z]{1,}|[А-ЯA-Z][\x27а-яa-z]{1,}\-([А-ЯA-Z][\x27а-яa-z]{1,}|(оглы)|(кызы)))\040[А-ЯA-Z][\x27а-яa-z]{1,}(\040[А-ЯA-Z][\x27а-яa-z]{1,})?$/;
+          re.test(value) ? setFirstOrLastNameError(false) : setFirstOrLastNameError(true);
+          break;
+        case 'isPhone':
+          // const req = /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/;
+          const req = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+          req.test(value) ? setPhoneError(false) : setPhoneError(true);
+          break;
+        case 'isEmail':
+          const reqe = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+          reqe.test(String(value).toLowerCase()) ? setEmailError(false) : setEmailError(true);
+          break;
+      }
+    }
+  }, [value]);
+
+  useEffect(() => {
+    if (isEmpty || minLengthError || maxLengthError || firstOrLastNameError || phoneError || emailError) {
+      setInputValid(false);
+    } else {
+      setInputValid(true);
+    }
+  }, [isEmpty, minLengthError, maxLengthError, firstOrLastNameError, phoneError, emailError]);
+
+  return {
+    isEmpty,
+    minLengthError,
+    maxLengthError,
+    firstOrLastNameError,
+    phoneError,
+    emailError,
+    inputValid,
+  };
+};
+
+const useInput = (initialValue, validations) => {
+  const [value, setValue] = useState(initialValue);
+  const [isDirty, setDirty] = useState(false);
+  const valid = useValidation(value, validations);
+
+  const onChange = (e) => { setValue(e.target.value); };
+  const onBlur = () => { setDirty(true); };
+
+  return {
+    value,
+    onChange,
+    onBlur,
+    isDirty,
+    ...valid,
+  };
+};
+
+const mockObj_orderContract = []; // объект собирает данные из формы
+
+const CartForm = () => {
+  const firstOrLastName = useInput('', { isEmpty: true, minLength: 2, maxLength: 100, isfirstOrLastName: true });
+  const address = useInput('', { isEmpty: true });
+  const phone = useInput('', { isEmpty: true, maxLength: 10, isPhone: true });
+  const email = useInput('', { isEmpty: true, minLength: 3, maxLength: 100, isEmail: true });
+
+  const [btnError, setBtnError] = useState(true);
+  const [statusOrder, setStatusOrder] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    if (!firstOrLastName.inputValid || !address.inputValid || !phone.inputValid || !email.inputValid) {
+      setBtnError(false);
+    } else {
+      setBtnError(true);
+      setStatusOrder(true);
+
+      mockObj_orderContract.push({ // объект собирает данные из формы
+        name: firstOrLastName.value,
+        email: email.value,
+        phone: phone.value,
+        total_sum: 'decimal',
+        final_sum: 'decimal',
+        address: address.value,
+        comment: 'String',
+        code: 'string',
+        items: [
+          {
+            item_id: 'integer',
+            count: 'integer',
+            size: 'string',
+            color: 'string',
+            price: 'integer',
+          },
+        ],
+      });
+    }
+  };
+
+  // console.log(mockObj_orderContract);
+
+  return (
+    <div className={styles.form}>
+      <form action="#" id="form" className={styles.form_body}>
+        <Title cn={styles.form__title} text="ещё совсем чуть-чуть и мерч твой" sqColor="green" />
+        <div className={styles.form_wrapper}>
+          <div className={styles.form_wrapper__col}>
+            <div className={styles.form__item}>
+              <label
+                htmlFor="forName"
+                className={`
+                  ${styles.form__label}
+                  ${(!btnError && firstOrLastName.isEmpty) && styles.form__label_error_message}
+                `}
+              >
+                Имя и фамилия *
+              </label>
+              <input
+                onChange={(e) => firstOrLastName.onChange(e)}
+                onBlur={(e) => firstOrLastName.onBlur(e)}
+                value={firstOrLastName.value}
+                id="forName"
+                type="name"
+                name="firstOrLastName"
+                className={`
+                  ${styles.form__input}
+                  ${(firstOrLastName.isDirty && firstOrLastName.isEmpty) && styles.form__input_error}
+                  ${(firstOrLastName.isDirty && firstOrLastName.minLengthError) && styles.form__input_error}
+                  ${(firstOrLastName.isDirty && firstOrLastName.maxLengthError) && styles.form__input_error}
+                  ${(firstOrLastName.isDirty && firstOrLastName.firseNameError) && styles.form__input_error}
+                  ${(!btnError && firstOrLastName.isEmpty) && styles.form__input_error}
+                `}
+              />
+            </div>
+            <div className={styles.form__item}>
+              <label
+                htmlFor="forAddress"
+                className={`
+                  ${styles.form__label}
+                  ${(!btnError && address.isEmpty) && styles.form__label_error_message}
+                `}
+              >
+                Адрес доставки *
+              </label>
+              <input
+                onChange={(e) => address.onChange(e)}
+                onBlur={(e) => address.onBlur(e)}
+                value={address.value}
+                id="forAddress"
+                type="text"
+                name="address"
+                className={`
+                  ${styles.form__input}
+                  ${(address.isDirty && address.isEmpty) && styles.form__input_error}
+                  ${(!btnError && address.isEmpty) && styles.form__input_error}
+                `}
+              />
+            </div>
+            <div className={styles.form__item}>
+              <label
+                htmlFor="forPhone"
+                className={`
+                  ${styles.form__label}
+                  ${(!btnError && phone.isEmpty) && styles.form__label_error_message}
+                `}
+              >
+                Телефон *
+              </label>
+              <input
+                onChange={(e) => phone.onChange(e)}
+                onBlur={(e) => phone.onBlur(e)}
+                value={phone.value}
+                id="forPhone"
+                type="tel"
+                name="phone"
+                className={`
+                  ${styles.form__input}
+                  ${(phone.isDirty && phone.isEmpty) && styles.form__input_error}
+                  ${(phone.isDirty && phone.phoneError) && styles.form__input_error}
+                  ${(!btnError && phone.isEmpty) && styles.form__input_error}
+                `}
+              />
+            </div>
           </div>
-          <div className={styles.form__item}>
-            <label htmlFor="forAddress" className={styles.form__label}>Адрес доставки *</label>
-            <input id="forAddress" type="text" name="address" className={styles.form__input} />
-          </div>
-          <div className={styles.form__item}>
-            <label htmlFor="forPhone" className={styles.form__label}>Телефон *</label>
-            <input id="forPhone" type="text" name="phone" className={styles.form__input} />
+          <div className={styles.form_wrapper__col}>
+            <div className={styles.form__item}>
+              <label
+                htmlFor="forEmail"
+                className={`
+                  ${styles.form__label} 
+                  ${(!btnError && email.isEmpty) && styles.form__label_error_message}
+                `}
+              >
+                E-mail *
+              </label>
+              <input
+                onChange={(e) => email.onChange(e)}
+                onBlur={(e) => email.onBlur(e)}
+                value={email.value}
+                id="forEmail"
+                type="email"
+                name="email"
+                className={`
+                  ${styles.form__input}
+                  ${(email.isDirty && email.isEmpty) && styles.form__input_error}
+                  ${(email.isDirty && email.minLengthError) && styles.form__input_error}
+                  ${(email.isDirty && email.emailError) && styles.form__input_error}
+                  ${(!btnError && email.isEmpty) && styles.form__input_error}
+                `}
+              />
+            </div>
+            <div className={styles.form__item}>
+              <label htmlFor="forMessage" className={styles.form__label}>Комментарий к заказу</label>
+              <textarea name="message" id="forMessage" className={styles.form__input} />
+            </div>
           </div>
         </div>
-        <div className={styles.form_wrapper__col}>
-          <div className={styles.form__item}>
-            <label htmlFor="forEmail" className={styles.form__label}>E-mail *</label>
-            <input id="forEmail" type="email" name="email" className={styles.form__input} />
-          </div>
-          <div className={styles.form__item}>
-            <label htmlFor="forMessage" className={styles.form__label}>Комментарий к заказу</label>
-            <textarea name="message" id="forMessage" className={styles.form__input} />
-          </div>
+        <div className={`${styles.form__item} ${styles.statusOrder_block}`}>
+          {/* <div className={styles.checkbox}>
+            <input id="formAgreement" type="checkbox" name="agreement" className={styles.checkbox__input} />
+            <label htmlFor="formAgreement" className={`${styles.checkbox__label}`}>
+              <span>
+                Согласен (согласна) с условиями
+                {' '}
+                <a href="#">пользовательского соглашения</a>
+              </span>
+            </label>
+          </div> */}
+          {/* <div className={styles.checkbox}>
+            <input id="formSubscribe" type="checkbox" name="subscribe" className={styles.checkbox__input} />
+            <label htmlFor="formSubscribe" className={styles.checkbox__label}>
+              <span>Согласен (согласна) получать информационные рассылки от Нетологии</span>
+            </label>
+          </div> */}
+          <button
+            onClick={handleClick}
+            type="button"
+            className={`${styles.form__button} btn`}
+          >
+            Оформить заказ
+          </button>
+          {statusOrder && (
+            <div className={styles.statusOrder}>
+              <p>Ваш заказ оформлен :)</p>
+              <p>
+                Письмо с подтверждением придёт на почту :
+                <span>
+                  {' '}
+                  { email.value}
+                </span>
+              </p>
+            </div>
+          )}
         </div>
-      </div>
-      <div className={styles.form__item}>
-        <div className={styles.checkbox}>
-          <input id="formAgreement" type="checkbox" name="agreement" className={styles.checkbox__input} />
-          <label htmlFor="formAgreement" className={styles.checkbox__label}>
-            <span>
-              Согласен (согласна) с условиями
-              {' '}
-              <a href="#">пользовательского соглашения</a>
-            </span>
-          </label>
-        </div>
-        <div className={styles.checkbox}>
-          <input id="formSubscribe" type="checkbox" name="subscribe" className={styles.checkbox__input} />
-          <label htmlFor="formSubscribe" className={styles.checkbox__label}>
-            <span>Согласен (согласна) получать информационные рассылки от Нетологии</span>
-          </label>
-        </div>
-        <button type="submit" className={`${styles.form__button} btn`}>Оформить заказ</button>
-      </div>
-    </form>
-  </div>
-);
+      </form>
+    </div>
+  );
+};
 
 export default CartForm;
