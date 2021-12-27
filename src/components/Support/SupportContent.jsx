373fc8ee09b12f7de68b5ef62e5e0f287152a134
kxrxll <kovalyovkirill@gmail.com>
2021-12-27 14:14:26 +0300

    help_center_animations

---
+++ src/components/Support/SupportContent.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import styles from './support.module.css';
+
+const SupportContent = ({
+  id, question, answer, isOpen, onClick,
+}) => (
+  <>
+    <button
+      className={isOpen ? styles.supportQuestionsOpen : styles.supportQuestions}
+      id={id}
+      type="button"
+      onClick={() => onClick(id)}
+    >
+      {question}
+    </button>
+    {isOpen
+      ? (
+        <div className={styles.supportAnswerContent}>
+          <div className={styles.supportAnswer} id="answer">
+            <p>{answer}</p>
+          </div>
+          <div className={styles.supportBackground} />
+        </div>
+      ) : false}
+  </>
+);
+
+export default SupportContent;
